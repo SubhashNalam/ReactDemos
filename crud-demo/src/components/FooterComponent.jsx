db4a95a79dbf3b9f1c6c3592ea628b1d91b8d43a
SubhashNalam <subhashnalam@gmail.com>
2021-07-24 09:21:27 +0530

    newly updated

--- crud-demo/src/components/FooterComponent.jsx
+++ crud-demo/src/components/FooterComponent.jsx
@@ -13,7 +13,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className="footer">
-                    <span className="text-muted">All Rights Reserved 2020 @Sangeetaj</span>
+                    <span className="text-muted">All Rights Reserved 2020 @Subhash Nalam</span>
                 </footer>
             </div>
         )
