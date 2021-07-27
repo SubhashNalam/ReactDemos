a0f47810b66783a76f5245aa3395b35c4613bb5b
SubhashNalam <subhashnalam@gmail.com>
2021-07-27 12:17:06 +0530

    update

--- redux-crud-demo/src/components/FooterComponent.jsx
+++ redux-crud-demo/src/components/FooterComponent.jsx
@@ -13,7 +13,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2021 @SangeetaJ</span>
+                    <span className="text-muted">All Rights Reserved 2021 @Subhash Nalam</span>
                 </footer>
             </div>
         )
